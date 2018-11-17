e44164b54b1666cd54e044e3b85aca8b14928774
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-11-17 14:28:40 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,43 @@ require 'pry'
 
 class Scraper
 
+
   def self.scrape_index_page(index_url)
-    
-  end
+    doc = open(index_url)
+    student_list = Nokogiri::HTML(doc)
+
+    students = []
+
+    roster_cards = student_list.css(".student-card")
+    roster_cards.each do |roster_card|
+      hash = {
+        name: roster_card.css(".student-name").text,
+        location: roster_card.css(".student-location").text,
+        profile_url: roster_card.css("a")[0]["href"]
+      }
+      students << hash
+    end
+    students
+    end
+
+
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    doc = open(profile_url)
+    student_profile = Nokogiri::HTML(doc)
 
+    profile = {}
+    social_icons = student_profile.css(".social-icon-container a")
+    social_icons.each do |social_icon|
+    link = social_icon.attribute("href").text
+          profile[:twitter] = link if link.include?("twitter")
+          profile[:linkedin] = link if link.include?("linkedin")
+          profile[:github] = link if link.include?("github")
+          profile[:blog] = link if social_icon.css("img").attribute("src").text.include?("rss")
 end
 
+    profile[:profile_quote] = student_profile.css("div.profile-quote").text
+    profile[:bio] = student_profile.css("div.bio-content .description-holder p").text
+    profile
+  end
+end
